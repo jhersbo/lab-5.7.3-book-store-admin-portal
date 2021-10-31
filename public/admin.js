c7737e77aa2f4421f5476c0a654931644c1379e4
jhersbo <jhersbo@gmail.com>
2021-10-31 11:50:57 -0500

    progress

--- public/admin.js
+++ public/admin.js
@@ -1,13 +1,20 @@
-let response = await fetch('http://127.0.0.1:3000//listBooks',{
+
+async function bookList(){
+    let response = await fetch('http://127.0.0.1:3000//listBooks',{
     method: 'GET',
     headers: {
         'Content-Type': 'application/JSON'
     }
 })
+    //Not sure why the request is not working.
 
-let listBooks = await response.json();
-console.log(listBooks)
+    let listBooks = await response.json();
+    console.log(listBooks)
+}
 
-let bookTiles = document.querySelectorAll()
+
+
+
+
 
 
